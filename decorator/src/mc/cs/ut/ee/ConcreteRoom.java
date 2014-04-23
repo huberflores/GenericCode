ada28cb05c5a7ca0cf94ffeba62caa0706cdaee7
Huber Flores <kyoraul@gmail.com>
2014-04-23 09:37:54 +0300

    decorator pattern

---
+++ decorator/src/mc/cs/ut/ee/ConcreteRoom.java
@@ -0,0 +1,10 @@
+package mc.cs.ut.ee;
+
+public class ConcreteRoom implements Room {
+
+	  @Override
+	  public String showRoom() {
+	    return "Concrete Room";
+	  }
+
+}
---
+++ decorator/src/mc/cs/ut/ee/CurtainDecorator.java
@@ -0,0 +1,16 @@
+package mc.cs.ut.ee;
+
+public class CurtainDecorator extends RoomDecorator {
+
+	  public CurtainDecorator (Room specialRoom) {
+	    super(specialRoom);
+	  }
+
+	  public String showRoom() {
+	    return specialRoom.showRoom() + addCurtains();
+	  }
+
+	  private String addCurtains() {
+	    return " + Red Curtains";
+	  }
+	}
