6383ea02adb490540f2018b81db2d8d0f538f413
Huber Flores <kyoraul@gmail.com>
2015-01-12 13:38:07 +0200

    auto scaling routines

---
+++ JavaAmazonEC2/ec2-tools/src/ee/ut/cs/ec2tools/core/ExecuteCommand.java
@@ -0,0 +1,44 @@
+/*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* Please send inquiries to huber AT ut DOT ee
+*/
+
+package ee.ut.cs.ec2tools.core;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class ExecuteCommand {
+
+	public static String executeCommand(String command) {
+		System.out.println("COMMAND: " + command);
+		StringBuilder tmp = new StringBuilder();
+		try {
+			ProcessBuilder pb = new ProcessBuilder(
+					"/bin/bash", "-c", command
+					);
+			Process p = pb.start();
+	        p.waitFor();
+	        if(p != null){
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(p.getInputStream()));
+				String line;
+				while( (line = reader.readLine()) != null){
+					tmp.append(line).append("\n");
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}	
+		return tmp.toString();
+	}
+	
+}
