c4218e41af83739069212a84abae8d693c3f4d9d
Huber Flores <kyoraul@gmail.com>
2015-01-13 00:09:46 +0200

    instance information can be retrieved from EC2

--- JavaAmazonEC2/ec2-tools/src/ee/ut/cs/ec2tools/commons/Utilities.java
+++ JavaAmazonEC2/ec2-tools/src/ee/ut/cs/ec2tools/commons/Utilities.java
@@ -1,6 +1,19 @@
 package ee.ut.cs.ec2tools.commons;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class Utilities {
 
 	public static String deployment = "deployment.properties";
+	
+	
+	public static <T> void iterateList(List<T> list){
+		Iterator<T> iterator = list.iterator();
+		
+		while (iterator.hasNext()){
+			System.out.println(iterator.next());
+		}
+	}
+	
 }
